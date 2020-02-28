bee38881004d26f598a54b229600d50907586837
MaxHand <rukav77@gmail.com>
2020-02-29 02:50:23 +0500

    start

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -2,28 +2,10 @@
   <div id="wrapper">
     <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
+      <div>
+        <h1>START PAGE</h1>
         <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-          <router-link to="/my_app"><button class="alt">go to app</button></router-link>
+          <router-link to="/my_app"><button class="alt"><h2>My App</h2></button></router-link>
         </div>
       </div>
     </main>
